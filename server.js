51c56296cbe64cb5140c651d59fd690a4c24b448
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 22:13:43 -0500

    server.js updated: prompts removed

--- server.js
+++ server.js
@@ -3,128 +3,102 @@ const inquirer = require("inquirer");
 require("console.table");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "employee_db",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employee_db",
 });
 connection.query = util.promisify(connection.query);
 
 connection.connect(function (err) {
-  if (err) throw err;
+    if (err) throw err;
 });
 
 function loadPrompts() {
-  prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What would you like to do?",
-      choices: [
+    prompt([
         {
-          name: "View All Employees",
-          value: "VIEW_EMPLOYEES",
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES",
+                },
+
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE",
+                },
+
+                {
+                    name: "Update Employee",
+                    value: "UPDATE_EMPLOYEE",
+                },
+
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES",
+                },
+
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE",
+                },
+
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS",
+                },
+
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT",
+                },
+
+                {
+                    name: "Quit",
+                    value: "QUIT",
+                },
+            ],
         },
+    ]).then((res) => {
+        let choice = res.choice;
 
-        {
-          name: "Add Employee",
-          value: "ADD_EMPLOYEE",
-        },
+        switch (choice) {
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
 
-        {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE",
-        },
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
 
-        {
-          name: "Update Employee",
-          value: "UPDATE_EMPLOYEE",
-        },
+            case "UPDATE_EMPLOYEE":
+                updateEmployee();
+                break;
 
-        {
-          name: "View All Roles",
-          value: "VIEW_ROLES",
-        },
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
 
-        {
-          name: "Add Role",
-          value: "ADD_ROLE",
-        },
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
 
-        {
-          name: "Remove Role",
-          value: "REMOVE_ROLE",
-        },
+            case "VIEW_ROLES":
+                viewRoles();
+                break;
 
-        {
-          name: "View All Departments",
-          value: "VIEW_DEPARTMENTS",
-        },
+            case "ADD_ROLE":
+                addRole();
+                break;
 
-        {
-          name: "Add Department",
-          value: "ADD_DEPARTMENT",
-        },
-
-        {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT",
-        },
-
-        {
-          name: "Quit",
-          value: "QUIT",
-        },
-      ],
-    },
-  ]).then((res) => {
-    let choice = res.choice;
-
-    switch (choice) {
-      case "VIEW_EMPLOYEES":
-        viewEmployees();
-        break;
-
-      case "ADD_EMPLOYEE":
-        addEmployee();
-        break;
-
-      case "REMOVE_EMPLOYEE":
-        removeEmployee();
-        break;
-
-      case "UPDATE_EMPLOYEE":
-        updateEmployee();
-        break;
-
-      case "VIEW_DEPARTMENTS":
-        viewDepartments();
-        break;
-
-      case "ADD_DEPARTMENT":
-        addDepartment();
-        break;
-
-      case "REMOVE_DEPARTMENT":
-        removeDepartment();
-        break;
-
-      case "VIEW_ROLES":
-        viewRoles();
-        break;
-
-      case "ADD_ROLE":
-        addRole();
-        break;
-
-      case "REMOVE_ROLE":
-        removeRole();
-        break;
-
-      default:
-        quit();
-    }
-  });
+            default:
+                quit();
+        }
+    });
 }
 
 loadPrompts();
+
