2c1667b3ab352da51f92e30cf19ef06c17a80739
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 20:58:24 -0500

    db/schema.sql updated: role table created

--- db/schema.sql
+++ db/schema.sql
@@ -9,5 +9,16 @@ USE employee_db;
 -- Creates a ' department ' table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
+);
+
+-- Creates a ' role ' table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
