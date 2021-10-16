11f88b2fe546f92e5f9811a44ed8a8131b3e3e66
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 21:14:56 -0500

    db/seeds.sql updated: values added to employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,10 +4,10 @@ USE employee_db;
 -- Inserts new values into ' department ' table
 INSERT INTO department (name)
 VALUES 
-("Sales"),
-("Finance"),
-("Engineering")
-("Accounting");
+    ("Sales"),
+    ("Finance"),
+    ("Engineering")
+    ("Accounting");
 
 -- Inserts new values into ' role ' table
 INSERT INTO role
@@ -21,3 +21,16 @@ VALUES
     ("Software Engineer", 120000, 3),
     ("Account Manager", 160000, 4),
     ("Accountant", 125000, 4);
+
+-- Inserts new values into ' employee ' table
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Raul", "Zavaleta", 1, NULL),
+    ("Maria", "Zavaleta", 2, 1),
+    ("Jr", "Zavaleta", 3, NULL),
+    ("Francelly", "Zavaleta", 4, 3),
+    ("Osvaldo", "Zavaleta", 5, NULL),
+    ("Omar", "Zavaleta", 6, 5),
+    ("Dylan", "Salcedo", 7, NULL),
+    ("Lilly", "Perez", 8, 7);
