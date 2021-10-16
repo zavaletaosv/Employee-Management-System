7543b0189be7e8ca636e3c3c4316df166e757c91
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 21:30:24 -0500

    server.js updated: connection added

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-require("dotenv");
-const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employee_db",
+});
+connection.query = util.promisify(connection.query);
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
