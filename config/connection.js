d443e15f21727aab71f3aae583b3097e26c2f52b
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 20:50:48 -0500

    config/connection.js updated: connection created

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employee_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
