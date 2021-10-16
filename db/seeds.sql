00defef471e5cd41b63fa5c19c262f51409637df
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 21:10:44 -0500

    db/seeds.sql updated: values added to role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,3 +8,16 @@ VALUES
 ("Finance"),
 ("Engineering")
 ("Accounting");
+
+-- Inserts new values into ' role ' table
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Sales Manager", 125000, 1),
+    ("Salesperson", 80000, 1),
+    ("Finance Manager", 145000, 2),
+    ("Finance Assistant", 50000, 2),
+    ("Lead Engineer", 150000, 3),
+    ("Software Engineer", 120000, 3),
+    ("Account Manager", 160000, 4),
+    ("Accountant", 125000, 4);
