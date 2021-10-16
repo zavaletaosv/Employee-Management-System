0fae52f6833fa8215ea0e146610013eb8e1a1d6f
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-15 21:02:21 -0500

    db/schema.sql updated: employee table created

--- db/schema.sql
+++ db/schema.sql
@@ -22,3 +22,14 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+-- Creates an ' employee ' table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    ON DELETE SET NULL
+);
